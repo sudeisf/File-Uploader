acdffc7362d590e6b8f50e473621f74356ae81e8
sudeis <sudeisfed@gmail.com>
2024-12-19 20:08:24 +0300

    editnitg some configurations

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,31 @@
-const passport  = require('passport');
+
 const Stratagy  = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { PrismaClient }  = require('@prisma/client');
+const prismaClient  = new PrismaClient();
+
+
+
+
 
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET
 }
 
-passport.use(new Stratagy(options, (payload, done, done) => {
-    return done(null, payload);
+passport.use(new Stratagy(options,async (payload, done, done) => {
+    try{
+        const user  = await prisma.user.findUnique({
+            where: {id: payload.sub}
+        });
+        if(!user){ 
+            return done(null, false, {message : 'User not found'});
+        }
+        return done(null, user);
+    }catch(e){
+        return done(e, false)
+    }
+
 }));
-
-
-
-
 
 module.exports = passport;
