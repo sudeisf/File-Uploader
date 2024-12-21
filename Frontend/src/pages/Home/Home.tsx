e8260d222e1489418dc4f4e923780ec82632e2da
sudeis <sudeisfed@gmail.com>
2024-12-21 12:30:01 +0300

    addijusting home ui

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,15 +1,13 @@
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 
 const Home = () => {
     return (
-        <div className="max-w-md mx-auto">
-            <h1 className="text-2xl font-bold mb-4 text-cyan-700">Home</h1>
-            <form action="">
-                <Input type="file" name="file" className="h-80" />
-                <img src="" alt="was image here" />
-
-            </form>
+        <div className="max-w-md mx-auto space-y-4">
+            <h1 className="text-2xl font-bold mb-4 text-cyan-700 capitalize">Uplod your files</h1> 
+            <p className="text-lg text-muted-foreground font-semibold font-mono">you can upload your files here</p>
+            <Button className="w-full bg-cyan-700">Upload</Button>
         </div>
     )
 }
