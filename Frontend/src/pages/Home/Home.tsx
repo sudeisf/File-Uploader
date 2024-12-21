51518141af45641b5a13c11da8af373ad2c0f322
sudeis <sudeisfed@gmail.com>
2024-12-22 00:04:04 +0300

    creating the upload page ui

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,13 +1,132 @@
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import{
+    Form,
+    FormItem,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import uploadIcon from "@/assets/icons/upload.svg";
+import { useState } from "react";
+import jpeg from "@/assets/icons/jpg-svgrepo-com.svg";
+
+
+const fileLimit = 2 * 1024 * 1024; // MB
+
+const formSchema =z.object({file: z.instanceof(File)
+.refine((file)=>{
+    [
+        "application/pdf",
+        "application/msword",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "application/vnd.oasis.opendocument.text",
+        "text/plain",
+        "application/vnd.oasis.opendocument.presentation",
+        "application/vnd.oasis.opendocument.spreadsheet",
+        "application/vnd.oasis.opendocument.graphics",
+        "image/jpeg",
+        "image/png",
+        "image/gif",
+        "image/bmp",
+        "image/webp",
+        "image/svg+xml"
+    ].includes(file.type), {
+        message: "Invalid file type"
+    }
+}).refine((file) => file.size <= fileLimit, {
+    message: "File size too large"
+})
+});
 
 
 const Home = () => {
+    const [file, setFile] = useState<File | null>(null);
+
+    const form  = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema)
+    });
+
+    function onSubmit(data: { file: File }) {
+        console.log("Selected file:", data.file);
+    }
+    function onFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const file = event.target.files?.[0];
+        if (file) {
+            setFile(file);
+          console.log("Selected file:", file);
+        }
+      }
+      
     return (
-        <div className="max-w-md mx-auto space-y-4">
-            <h1 className="text-2xl font-bold mb-4 text-cyan-700 capitalize">Uplod your files</h1> 
-            <p className="text-lg text-muted-foreground font-semibold font-mono">you can upload your files here</p>
-            <Button className="w-full bg-cyan-700">Upload</Button>
+        <div>
+        <h1 className="text-2xl font-bold text-center mb-5 uppercase">Upload a file</h1>
+        
+       <Form {...form}>
+        <form className='flex flex-col items-start min-w-sm max-w-2xl mx-auto
+            '
+          onSubmit={form.handleSubmit(onSubmit)}
+         >  
+         <FormField
+          control={form.control}    
+          name="file" 
+          render={({ field }) => (   
+            <FormItem className='flex flex-col items-center drop-shadow-sm border-[3px] border-dotted border-gray-300 p-4 w-full text-white rounded-lg'>
+              <FormLabel className='text-start font-bold space-y-4 py-8 flex flex-col items-center text-black cursor-pointer'>
+              <img src={uploadIcon} alt="" className="w-20"/>
+                <h1 className="text-2xl font-semibold text-center">Create or Import a custom Document</h1>
+                <div 
+                className="flex flex-col items-center max-w-md space-y-2">
+                    <p className="text-md font-normal ">Minimum file size: 2MB</p>
+                    <p className="text-md font-normal  capitalize">supports : images and documents</p>
+                </div>
+                 </FormLabel>
+              <FormControl> 
+                <Input     
+                  type="file"
+                  onChange={(e) => field.onChange(onFileChange(e))}
+                  className="hidden"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        </form>
+       </Form>
+
+          <div  className='flex flex-col items-start min-w-sm max-w-2xl mx-auto ' >
+              <h1 className="px-4 py-2 text-xl font-bold mt-4  w-full text-start">Progress</h1>
+                {
+                    file && (
+                        <div className="flex flex-col items-start w-full ">
+                            <div className="text-md flex space-x-4 p-4 items-start border-[1px] mt-4 rounded-lg w-full text-start">
+                              <div className="my-auto">
+                                <img src={jpeg} alt="type-image" className="w-10"/>
+                              </div>
+                                    <div className="flex flex-col  space-y-1">
+                                        <h1 className="text-sm font-semibold p-0"> {file.name}</h1>
+                                        <div className="flex items-center max-w-md space-x-4">
+                                            <p className="text-sm font-normal uppercase">{file.type.split('/')[1]}</p>
+                                            <p className="text-md font-normal"> {(file.size / 1024).toFixed(2)} KB</p>
+                                        </div>
+                                </div>
+                            </div>
+                            
+                        </div>      
+                    )
+                }
+          </div>
+
+
+
         </div>
     )
 }
